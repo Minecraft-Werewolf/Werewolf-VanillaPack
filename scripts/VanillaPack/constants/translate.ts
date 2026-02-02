7fc7f8c3bc3c2a6024f90cc62c47a8394c6cf238
shizuku86 <shizuku86710@gmail.com>
2026-02-02 18:11:36 +0900

    changed translate ids

--- scripts/VanillaPack/constants/translate.ts
+++ scripts/VanillaPack/constants/translate.ts
@@ -1,63 +1,63 @@
 export const WEREWOLF_STANDARDROLES_TRANSLATE_IDS = {
-    FACTION_NAME_VILLAGER: "werewolf-standardroles.faction.name.villager",
-    FACTION_NAME_WEREWOLF: "werewolf-standardroles.faction.name.werewolf",
+    FACTION_NAME_VILLAGER: "werewolf-vanillapack.faction.name.villager",
+    FACTION_NAME_WEREWOLF: "werewolf-vanillapack.faction.name.werewolf",
 
-    FACTION_DESCRIPTION_VILLAGER: "werewolf-standardroles.faction.description.villager",
-    FACTION_DESCRIPTION_WEREWOLF: "werewolf-standardroles.faction.description.werewolf",
+    FACTION_DESCRIPTION_VILLAGER: "werewolf-vanillapack.faction.description.villager",
+    FACTION_DESCRIPTION_WEREWOLF: "werewolf-vanillapack.faction.description.werewolf",
 
-    FACTION_VICTORYCONDITION_VILLAGER: "werewolf-standardroles.faction.victorycondition.villager",
-    FACTION_VICTORYCONDITION_WEREWOLF: "werewolf-standardroles.faction.victorycondition.werewolf",
+    FACTION_VICTORYCONDITION_VILLAGER: "werewolf-vanillapack.faction.victorycondition.villager",
+    FACTION_VICTORYCONDITION_WEREWOLF: "werewolf-vanillapack.faction.victorycondition.werewolf",
 
-    ROLE_NAME_VILLAGER: "werewolf-standardroles.role.name.villager",
-    ROLE_NAME_SEER: "werewolf-standardroles.role.name.seer",
-    ROLE_NAME_MEDIUM: "werewolf-standardroles.role.name.medium",
-    ROLE_NAME_KNIGHT: "werewolf-standardroles.role.name.knight",
-    ROLE_NAME_WEREWOLF: "werewolf-standardroles.role.name.werewolf",
-    ROLE_NAME_GREATWOLF: "werewolf-standardroles.role.name.greatwolf",
-    ROLE_NAME_MADMAN: "werewolf-standardroles.role.name.madman",
+    ROLE_NAME_VILLAGER: "werewolf-vanillapack.role.name.villager",
+    ROLE_NAME_SEER: "werewolf-vanillapack.role.name.seer",
+    ROLE_NAME_MEDIUM: "werewolf-vanillapack.role.name.medium",
+    ROLE_NAME_KNIGHT: "werewolf-vanillapack.role.name.knight",
+    ROLE_NAME_WEREWOLF: "werewolf-vanillapack.role.name.werewolf",
+    ROLE_NAME_GREATWOLF: "werewolf-vanillapack.role.name.greatwolf",
+    ROLE_NAME_MADMAN: "werewolf-vanillapack.role.name.madman",
 
-    ROLE_DESCRIPTION_VILLAGER: "werewolf-standardroles.role.description.villager",
-    ROLE_DESCRIPTION_SEER: "werewolf-standardroles.role.description.seer",
-    ROLE_DESCRIPTION_MEDIUM: "werewolf-standardroles.role.description.medium",
-    ROLE_DESCRIPTION_KNIGHT: "werewolf-standardroles.role.description.knight",
-    ROLE_DESCRIPTION_WEREWOLF: "werewolf-standardroles.role.description.werewolf",
-    ROLE_DESCRIPTION_GREATWOLF: "werewolf-standardroles.role.description.greatwolf",
-    ROLE_DESCRIPTION_MADMAN: "werewolf-standardroles.role.description.madman",
+    ROLE_DESCRIPTION_VILLAGER: "werewolf-vanillapack.role.description.villager",
+    ROLE_DESCRIPTION_SEER: "werewolf-vanillapack.role.description.seer",
+    ROLE_DESCRIPTION_MEDIUM: "werewolf-vanillapack.role.description.medium",
+    ROLE_DESCRIPTION_KNIGHT: "werewolf-vanillapack.role.description.knight",
+    ROLE_DESCRIPTION_WEREWOLF: "werewolf-vanillapack.role.description.werewolf",
+    ROLE_DESCRIPTION_GREATWOLF: "werewolf-vanillapack.role.description.greatwolf",
+    ROLE_DESCRIPTION_MADMAN: "werewolf-vanillapack.role.description.madman",
 
     WEREWOLF_GAME_RESULT_WEREWOLF_FACTION_VICTORY_TITLE:
-        "werewolf-standardroles.game.result.werewolffaction.victory.title",
+        "werewolf-vanillapack.game.result.werewolffaction.victory.title",
     WEREWOLF_GAME_RESULT_WEREWOLF_FACTION_VICTORY_MESSAGE:
-        "werewolf-standardroles.game.result.werewolffaction.victory.message",
+        "werewolf-vanillapack.game.result.werewolffaction.victory.message",
     WEREWOLF_GAME_RESULT_VILLAGER_FACTION_VICTORY_TITLE:
-        "werewolf-standardroles.game.result.villagerfaction.victory.title",
+        "werewolf-vanillapack.game.result.villagerfaction.victory.title",
     WEREWOLF_GAME_RESULT_VILLAGER_FACTION_VICTORY_MESSAGE:
-        "werewolf-standardroles.game.result.villagerfaction.victory.message",
+        "werewolf-vanillapack.game.result.villagerfaction.victory.message",
 
-    SKILL_NAME_SEER_DIVINATION: "werewolf-standardroles.skill.name.seer.divination",
-    SKILL_NAME_KNIGHT_PROTECT: "werewolf-standardroles.skill.name.knight.protect",
-    SKILL_NAME_MEDIUM_CLAIRVOYANCE: "werewolf-standardroles.skill.name.medium.clairvoyance",
+    SKILL_NAME_SEER_DIVINATION: "werewolf-vanillapack.skill.name.seer.divination",
+    SKILL_NAME_KNIGHT_PROTECT: "werewolf-vanillapack.skill.name.knight.protect",
+    SKILL_NAME_MEDIUM_CLAIRVOYANCE: "werewolf-vanillapack.skill.name.medium.clairvoyance",
 
-    SEER_SKILL_FORM_TITLE: "werewolf-standardroles.skill.seer.form.title",
-    SEER_SKILL_FORM_DESCRIPTION: "werewolf-standardroles.skill.seer.form.description",
+    SEER_SKILL_FORM_TITLE: "werewolf-vanillapack.skill.seer.form.title",
+    SEER_SKILL_FORM_DESCRIPTION: "werewolf-vanillapack.skill.seer.form.description",
     SEER_SKILL_FORM_TARGET_DROPDOWN_LABEL:
-        "werewolf-standardroles.skill.seer.form.target.dropdown.label",
-    SEER_SKILL_DIVINATION_RESULT: "werewolf-standardroles.skill.seer.divination.result",
+        "werewolf-vanillapack.skill.seer.form.target.dropdown.label",
+    SEER_SKILL_DIVINATION_RESULT: "werewolf-vanillapack.skill.seer.divination.result",
     SEER_SKILL_NO_AVAILABLE_TARGETS_ERROR:
-        "werewolf-standardroles.skill.seer.noAvailableTargets.error",
+        "werewolf-vanillapack.skill.seer.noAvailableTargets.error",
 
-    MEDIUM_SKILL_FORM_TITLE: "werewolf-standardroles.skill.medium.form.title",
-    MEDIUM_SKILL_FORM_DESCRIPTION: "werewolf-standardroles.skill.medium.form.description",
+    MEDIUM_SKILL_FORM_TITLE: "werewolf-vanillapack.skill.medium.form.title",
+    MEDIUM_SKILL_FORM_DESCRIPTION: "werewolf-vanillapack.skill.medium.form.description",
     MEDIUM_SKILL_FORM_TARGET_DROPDOWN_LABEL:
-        "werewolf-standardroles.skill.medium.form.target.dropdown.label",
-    MEDIUM_SKILL_CLAIRVOYANCE_RESULT: "werewolf-standardroles.skill.medium.clairvoyance.result",
+        "werewolf-vanillapack.skill.medium.form.target.dropdown.label",
+    MEDIUM_SKILL_CLAIRVOYANCE_RESULT: "werewolf-vanillapack.skill.medium.clairvoyance.result",
     MEDIUM_SKILL_NO_AVAILABLE_TARGETS_ERROR:
-        "werewolf-standardroles.skill.medium.noAvailableTargets.error",
+        "werewolf-vanillapack.skill.medium.noAvailableTargets.error",
 
-    KNIGHT_SKILL_FORM_TITLE: "werewolf-standardroles.skill.knight.form.title",
-    KNIGHT_SKILL_FORM_DESCRIPTION: "werewolf-standardroles.skill.knight.form.description",
+    KNIGHT_SKILL_FORM_TITLE: "werewolf-vanillapack.skill.knight.form.title",
+    KNIGHT_SKILL_FORM_DESCRIPTION: "werewolf-vanillapack.skill.knight.form.description",
     KNIGHT_SKILL_FORM_TARGET_DROPDOWN_LABEL:
-        "werewolf-standardroles.skill.knight.form.target.dropdown.label",
-    KNIGHT_SKILL_PROTECT_RESULT: "werewolf-standardroles.skill.knight.protect.result",
+        "werewolf-vanillapack.skill.knight.form.target.dropdown.label",
+    KNIGHT_SKILL_PROTECT_RESULT: "werewolf-vanillapack.skill.knight.protect.result",
     KNIGHT_SKILL_NO_AVAILABLE_TARGETS_ERROR:
-        "werewolf-standardroles.skill.knight.noAvailableTargets.error",
+        "werewolf-vanillapack.skill.knight.noAvailableTargets.error",
 };
